1ce7eaccb988c5703da4e9b0de5e7ca7fa38e8e9
Torens29 <44534255+Torens29@users.noreply.github.com>
2022-01-20 22:45:19 +0300

    после потери файла1

--- exec.php
+++ exec.php
@@ -1,3 +1,5 @@
+
+  
 <?php
 require_once __DIR__.'\SimpleXLSX.php';
 
@@ -35,6 +37,7 @@ echo '<br> VIDEO ' . $infoBeach[0];
         -loop 1 -t 5 -i img\\" . $enBeach . "2.jpg 
         -loop 1 -t 5 -i img\\" . $enBeach . "3.jpg 
         -loop 1 -t 5 -i img\\" . $enBeach . "4.jpg 
+
         -loop 1 -t 3 -i map\\" . $enBeach . "1.png
         -loop 1 -t 3 -i map\\" . $enBeach . "2.png
         -loop 1 -t 3 -i map\\" . $enBeach . "3.png
@@ -44,19 +47,14 @@ echo '<br> VIDEO ' . $infoBeach[0];
         drawtext=fontfile=/Library/Fonts/Arial.ttf:text='       в $infoBeach[1]':fontcolor=white:fontsize=24:x=(w-tw)/2:y=(h/PHI)+th+100,
         split[pre][pbv0];[pbv0]fifo[bv0]; 
         [pre]fade=t=in:st=0:d=1[v0]; 
-
         [1:v]drawtext=fontfile=/Library/Fonts/Arial.ttf:text='Длина береговой линии $infoBeach[2]':fontcolor=white:fontsize=24:x=(w-tw)/2:y=(h/PHI)+th,
         split=3[pbv1a][pbv1b][v1];[pbv1a]fifo[bv1a];[pbv1b]fifo[bv1b]; 
-
         [2:v]drawtext=fontfile=/Library/Fonts/Arial.ttf:text='THIRD TEXT':fontcolor=white:fontsize=24:x=(w-tw)/2:y=(h/PHI)+th,
         split=3[pbv2a][pbv2b][v2];[pbv2a]fifo[bv2a];[pbv2b]fifo[bv2b];
-
         [3:v]drawtext=fontfile=/Library/Fonts/Arial.ttf:text='FOURTH TEXT':fontcolor=white:fontsize=24:x=(w-tw)/2:y=(h/PHI)+th,
         split=3[pbv3a][pbv3b][v3];[pbv3a]fifo[bv3a];[pbv3b]fifo[bv3b]; 
-
         [4:v]drawtext=fontfile=/Library/Fonts/Arial.ttf:text='FIFTH TEXT':fontcolor=white:fontsize=24:x=(w-tw)/2:y=(h/PHI)+th,
         split[pbv4][v4];[pbv4]fifo[bv4]; 
-
         [5]zoompan=z='min(max(zoom,pzoom)+0.0015,1.5)':d=1:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)'[map1];
         [6]zoompan=z='min(max(zoom,pzoom)+0.0015,1.5)':d=1:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)'[map2];
         [7]zoompan=z='min(max(zoom,pzoom)+0.0015,1.5)':d=1:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)'[map3];
@@ -70,8 +68,30 @@ echo '<br> VIDEO ' . $infoBeach[0];
     
     $text =str_replace(array("\n\r","\r\n"), "", $comm);
     //  echo $text;
-    exec($text);
-    $addVoice = "ffmpeg -i $enBeach.mp4 -itsoffset 00:00:05 -i voice\\v1.ogg -map 0:v -map 1:a -y video\\$enBeach.mp4";
-    exec($addVoice);
+    // exec($text);
+    
+    $addVoice = "ffmpeg  -async 1 -i video\Yashmovyy+plyazh.mp4
+        -itsoffset 00:00:01 -i voice\\nameBeach.ogg 
+        -itsoffset 00:00:04 -i voice\\placeBeach.ogg 
+        -itsoffset 00:00:07 -i voice\\length.ogg 
+        -itsoffset 00:00:10 -i voice\\surface.ogg 
+        -itsoffset 00:00:10 -i voice\bottom.ogg 
+        -itsoffset 00:00:10 -i voice\\receive.ogg
+        -itsoffset 00:00:10 -i voice\score.ogg
+        -f lavfi -t 4 -i anullsrc=channel_layout=stereo:sample_rate=44100 
+        -f lavfi -t 7 -i anullsrc=channel_layout=stereo:sample_rate=44100 
+        -f lavfi -t 14 -i anullsrc=channel_layout=stereo:sample_rate=44100 
+
+        -filter_complex \"
+            [8:a][5:a]concat=v=0:a=1 [addSilence1],
+            [9:a][6:a]concat=v=0:a=1 [addSilence2],
+            [10:a][7:a]concat=v=0:a=1 [addSilence3],
+            
+            [0][1][2][3][4][addSilence1][addSilence2][addSilence3]amix=inputs=8\" 
+        -c:v copy -c:a aac  -y video\\test.mp4";
+
+
+    $text2= str_replace(array("\n\r","\r\n"), "", $addVoice);
+    exec($text2);
     unlink("$enBeach.mp4");
 }
