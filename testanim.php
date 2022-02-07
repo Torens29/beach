2bbf3a889140e0433e3bc61cb2c65bf147e74e3e
Torens29 <44534255+Torens29@users.noreply.github.com>
2022-02-07 12:33:39 +0300

    Update testanim.php

--- testanim.php
+++ testanim.php
@@ -1,19 +1,23 @@
 <?php
 
 $t;
+$text1[0] = "-f lavfi -i color=c=white:s=200x200  -loop 1 -i fly.png";
+$text1[1] = "[2:v]scale=-1:150[x2],
+             [1:v][x2]overlay=(W-w)/2:H[x12],
+             [x12]drawtext=fontfile=/Library/Fonts/Arial.ttf:text='Add text':fontcolor=black:
+                    fontsize=20:x=(w-text_w)/2:y=h-th[x1t2],
+             [x1t2]fade=t=in:0:50:alpha=1[X1];
+             [0:v][X1]overlay=(W-w)/2:(H-h)/2";
+
+$text2 = "[f2]fade=t=in:st=3:d=0.5:alpha=1[x22],
+    [de][x22]overlay=(W-w)/2+200:(H-h)/2+200";
+
 $cmd="ffmpeg 
--f lavfi -i color=c=white:s=1280x720 -loop 1 -i fly.png  
+    -f lavfi -i color=c=white:s=1280x720  $text1[0] 
     -filter_complex \"
+    $text1[1] \"
     
-    [1]scale=640:-1,
-    fade=t=in:0:50[x21],
-    [0][x21]overlay=(W-w)/2:(H-h)/2,trim=0:12
-    \"
+    -pix_fmt yuv420p -t 5 -s \"1280x720\" -c:a copy -y output.mp4";
 
-    -pix_fmt yuv420p -t 15 -c:a copy -y output.mp4";
-
-$text =str_replace(array("\n\r","\r\n"), "", $cmd);
-// [1]split[bv1a][1v] ; [bv1][title]concat=n=2\" :enable='between(t,1,3) 
+$text =str_replace(array("\n\r","\r\n"), "", $cmd); 
 exec($text);
-// [0] drawtext=fontfile=/Library/Fonts/Arial.ttf:text='Инфраструктура пляжа':fontcolor=black:fontsize=54:x=(w-tw)/2:y=(h/w)+th [title];
-    
