116de579417751a1d86fabab5d122b289bc03f79
Torens29 <44534255+Torens29@users.noreply.github.com>
2022-03-03 20:35:43 +0300

    подделка сбора инфы

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HEADER, false);
 $j=0;
+$mapTrue= false;
+
 foreach($beaches as $beach){
     $j++;
 
@@ -28,104 +30,106 @@ foreach($beaches as $beach){
     $s = translit($beach[0]);
     $enCity= translit($beach[1]);
     
+    //cбор координат
+
     $enBeach = str_replace(' ','+',$s);
     // $enBeach= $beach[1];
-    echo $beach[1] . $enCity . $beach[0] . $enBeach;
-    echo "<br>";
+    echo $beach[1] . $enCity . $beach[0] . $enBeach . "\n";
     $URL= "https://geocode-maps.yandex.ru/1.x/?apikey=28d74b24-f33d-4b35-8949-88142b0c9d92&geocode=Russia,+$enCity,+$enBeach&scale=1&results=1&format=json";
 
     curl_setopt($ch, CURLOPT_URL, $URL);
-
+    
     $json = curl_exec($ch);
-    if (curl_errno($ch)) {  
+    if (curl_errno($ch)) {  //ошибка в получении координат
         $info = curl_getinfo($ch);
         var_dump($info);
         echo 'Error:' . curl_error($ch);
     }
-    else{
+    else{ //получили координаты
         $fd = json_decode($json,false);
         // var_dump($fd);
-        echo "<br>";
-        if ($fd==null){
-            echo 'ERROR:' . $beach[1] . "<br";
+        echo "\n";
+        if ($fd==null){ // проблема с координатами
+            echo 'ERROR:' . $enBeach . "\n";
             file_put_contents('errorBeach.txt', $j . ": " . $enBeach .'- Error with coord'. PHP_EOL, FILE_APPEND);
             continue;
-        } //если все хорошо и все получили, то...
-        if ( count($fd->response->GeoObjectCollection->featureMember) == 0){continue;}
+        } 
+        else{ //получаем карту
+            if ( count($fd->response->GeoObjectCollection->featureMember) == 0) continue;
             else{
 
-            $coord =str_replace(' ', ',', $fd->response->GeoObjectCollection->featureMember[0]->GeoObject-> Point-> pos);
-            
-            echo "COORD of $beach[1]: ";
-            var_dump($coord);
+                $coord =str_replace(' ', ',', $fd->response->GeoObjectCollection->featureMember[0]->GeoObject-> Point-> pos);
+                
+                echo "COORD of $beach[0]: ";
+                var_dump($coord);
 
-            //receive map .PNG
-            $z = 0;
-            for ($i=1;$i<=3;$i++) {
-                switch($i){
-                    case 1: $z=10;
-                            break;
-                    case 2: $z=13;
-                            break;
-                    case 3: $z=16;
-                            break;
+                //receive map .PNG
+                $zoom = 0;
+                for ($z=1;$z<=3;$z++) {
+                    switch($z){
+                        case 1: $zoom=10;
+                                break;
+                        case 2: $zoom=13;
+                                break;
+                        case 3: $zoom=16;
+                                break;
+                    }
+
+                    $URL ="https://static-maps.yandex.ru/1.x/?ll=$coord&pt=$coord,org,l&size=1280,720&z=$zoom&l=map&key=APeD310BAAAALOt-HAMAAfLbAiZoUWW9QhK-Di0vA9V64lMAAAAAAAAAAADA-ZKiGOZHTc7Nt7dlEdOj78HURA%3D%3D";
+
+
+                    curl_setopt($ch, CURLOPT_URL, $URL);
+
+                    // var_dump($ch);
+                    $png = curl_exec($ch);
+
+                    if (curl_errno($ch)) {
+                        $info = curl_getinfo($ch);
+                        var_dump($info);
+
+                        echo 'Error:' . curl_error($ch);
+                    } else {
+                        // var_dump($png);
+                        $s = str_replace('"','',$enBeach);
+                        $str = "map/" . $s . $z . ".png";
+                        file_put_contents($str, $png);
+                        $mapTrue= true;
+                        
+                    }
+                }    
+
+                if ($mapTrue){
+                    //receive img (6)
+                    $imgArr = explode("\n", $beach[9]);
+                    for($i=0; $i<8;$i++){
+                        curl_setopt($ch, CURLOPT_URL, $imgArr[$i]);
+                            
+                        $img = curl_exec($ch);
+
+                        $src= "img\\" . $s . $i . ".jpg";
+                        file_put_contents($src, $img);
+                        try {
+                            $image  ->fromFile($src)
+                                    ->resize(1280,720)
+                                    -> toFile($src,'image/jpeg');
+                        } catch(Exception $err) {
+                            $msg= $err->getMessage();
+                            file_put_contents('errorBeach.txt', $j . ": " . $enBeach . "- ". $msg . PHP_EOL, FILE_APPEND);
+                            echo 'ERROR:' . $beach[1] . "\n";
+                            echo $msg;
+                            unlink($src);
+                            continue;
+                        }   
+                    }
+
+                    $nameBeach = str_replace(" ", "+",$beach[0]);
+                    file_put_contents('receive_img.txt', $j . " " . $s . " $nameBeach" . PHP_EOL, FILE_APPEND);
+                
                 }
-
-                $URL ="https://static-maps.yandex.ru/1.x/?ll=$coord&pt=$coord,org,l&size=1280,720&z=$z&l=map&key=APeD310BAAAALOt-HAMAAfLbAiZoUWW9QhK-Di0vA9V64lMAAAAAAAAAAADA-ZKiGOZHTc7Nt7dlEdOj78HURA%3D%3D";
-
-
-                curl_setopt($ch, CURLOPT_URL, $URL);
-
-                // var_dump($ch);
-                $png = curl_exec($ch);
-
-                if (curl_errno($ch)) {
-                    $info = curl_getinfo($ch);
-                    var_dump($info);
-
-                    echo 'Error:' . curl_error($ch);
-                } else {
-                    // var_dump($png);
-                    // $s = str_replace('+','',$enBeach);
-                    $str = "map/" . $enBeach . $i . ".png";
-                    file_put_contents($str, $png);
-                }
-            }    
- //receive img (6)
-            $imgArr = explode("\n", $beach[9]);
-            for($i=0; $i<8;$i++){
-             curl_setopt($ch, CURLOPT_URL, $imgArr[$i]);
-                    
-                $img = curl_exec($ch);
-
-                $src= "img\\" . $enBeach . $i . ".jpg";
-                file_put_contents($src, $img);
-                try {
-                    $image  ->fromFile($src)
-                            ->resize(1280,720)
-                            -> toFile($src,'image/jpeg');
-                } catch(Exception $err) {
-                    $msg= $err->getMessage();
-                    file_put_contents('errorBeach.txt', $j . ": " . $enBeach . "- ". $msg . PHP_EOL, FILE_APPEND);
-                    echo 'ERROR:' . $beach[1] . "<br";
-                    echo $msg;
-                    unlink($src);
-                    continue;
-                }   
-            }
-
-            $nameBeach = str_replace(" ", "+",$beach[0]);
-            file_put_contents('receive_img.txt', $j . " " . $enBeach . " $nameBeach" . PHP_EOL, FILE_APPEND);
-            // video($enBeach);
-            // in unlink() deleite all
-            // unlink("img\\" . $enBeach . "1.jpg");
-           
+            }           
         }
     }
-
-   
-    if($j==10) break;
-    
+    if($j>10) break;
 }   
 curl_close($ch);
 
